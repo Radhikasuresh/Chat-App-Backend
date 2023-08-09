f5cff307f62e7e693cf6f013425ad25c66a8bc1a
Radhikasuresh <radhikasuresh550@gmail.com>
2023-08-09 19:21:20 +0530

    /get

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+app.get("/",(req,res)=>{
+  res.send("Welcome to the Chat app");
+})
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
