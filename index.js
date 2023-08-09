41be1c2e5646b77fce65b6d575807daf73e996be
Radhikasuresh <radhikasuresh550@gmail.com>
2023-08-09 19:09:52 +0530

    final

--- index.js
+++ index.js
@@ -1,65 +1,36 @@
-// Libraray
 const express = require("express");
-const socketio = require("socket.io");
+const app = express();
 const http = require("http");
-const { addUser, removeUser, getUser, getRoomUsers } = require("./entity");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
 
-// Instances
-const app = express()
 const server = http.createServer(app);
-const io = socketio(server,{cors: { origin: '*' }})
 
-// End point
-app.get('/',(req,res) => {
-  res.json("Api is working");
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-// Socket
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-io.on('connect',(socket) => {
-  
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-  socket.on('join',({user,room},callback) => {
-    console.log(user,room)
-      const {response , error} = addUser({id: socket.id , user:user, room: room})
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-      console.log(response)
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
-      if(error) {
-        callback(error)
-        return;
-      }
-      socket.join(response.room);
-      socket.emit('message', { user: 'admin' , text: `Welcome ${response.user} ` });
-      socket.broadcast.to(response.room).emit('message', { user: 'admin', text : `${response.user} has joined` })
-
-      io.to(response.room).emit('roomMembers', getRoomUsers(response.room))
-  })
-
-  socket.on('sendMessage',(message,callback) => {
-    
-      const user = getUser(socket.id)
-
-      io.to(user.room).emit('message',{ user: user.user, text : message })
-
-      callback()
-  })
-
-
- 
-
-  socket.on('disconnect',() => {
-    console.log("User disconnected");
-    const user = removeUser(socket.id);
-
-    if(user) {
-      io.to(user.room).emit('message',{ user: 'admin', text : `${user.user} has left` })
-    }
-  })
-
-})
-
-
-
-
-server.listen(8000,() => console.log('Server started on 8000'))
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
